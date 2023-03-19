a4ddde678eba6c5431f9b38da05940444befb9d7
Patryk <kasprov.esc@gmail.com>
2023-03-19 21:11:32 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
 import Logo from './components/logo/Logo'
 import Navbar from './components/navbar/Navbar'
 import Calculator from './components/modals/Calculator'
 import Chat from './components/modals/Chat'
 import Strona from './components/pages/strona/Strona'
 import Contacts from './components/modals/Contacts'
+// import Kalendarz from './components/pages/Calendar/Kalendarz'
+
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 // import Note from './components/pages/Note/Note'
 // import Todo from './components/pages/Todo/Todo'
 // import Budget from './components/pages/Budget'
 // import Calendar from './components/pages/Calendar'
 import './style/index.css'
 
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+      title: "Big Meeting",
+      allDay: true,
+      start: new Date(2021, 6, 0),
+      end: new Date(2021, 6, 0),
+  },
+  {
+      title: "Vacation",
+      start: new Date(2021, 6, 7),
+      end: new Date(2021, 6, 10),
+  },
+  {
+      title: "Conference",
+      start: new Date(2021, 6, 20),
+      end: new Date(2021, 6, 23),
+  },
+];
 
 function App() {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    function handleAddEvent() {
+        
+        for (let i=0; i<allEvents.length; i++){
+
+            const d1 = new Date (allEvents[i].start);
+            const d2 = new Date(newEvent.start);
+            const d3 = new Date(allEvents[i].end);
+            const d4 = new Date(newEvent.end);
+      /*
+          console.log(d1 <= d2);
+          console.log(d2 <= d3);
+          console.log(d1 <= d4);
+          console.log(d4 <= d3);
+            */
+
+             if (
+              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
+                (d4 <= d3) )
+              )
+            {   
+                alert("CLASH"); 
+                break;
+             }
+    
+        }
+        
+        
+        setAllEvents([...allEvents, newEvent]);
+    }
   return (
       <div className="content">
         <Calculator />
@@ -22,7 +93,18 @@ function App() {
           <Logo />
           <Navbar />
         </section>
-        <Strona />
+        <h1>Calendar</h1>
+            <h2>Add New Event</h2>
+            <div>
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
+            </div>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+        {/* <Strona /> */}
         {/* <Note /> */}
         {/* <Todo /> */}
         {/* <Budget /> */}
---
+++ src/components/pages/Calendar/Kalendarz.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './calendar.css'
+
+export default function Kalendarz() {
+
+    return (
+        <p>tak</p>
+    )
+}
